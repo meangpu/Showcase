135bdfae11e44a310e70f71649ce3b402c2f9359
meangpu <51825872+meangpu@users.noreply.github.com>
2023-12-04 18:39:16 +0700

    update project

---
+++ showcase/src/pages/Project/AllProject.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./AllProject.css";
+import { projectData } from "../../../data.json";
+import GameCard from "./GameCard/GameCard";
+
+const AllGames = () => {
+  const Projects = projectData.map(
+    (
+      { name, des, img, video, githubLink, youtubeLink, demoLink, date },
+      index
+    ) => {
+      return (
+        <GameCard
+          key={index}
+          name={name}
+          des={des}
+          imageSrc={img}
+          videoSrc={video}
+          githubLink={githubLink}
+          demoLink={demoLink}
+          youtubeLink={youtubeLink}
+          date={date}
+          displayDemoBtn={true}
+        />
+      );
+    }
+  );
+
+  return (
+    <section>
+      <div className='container all_game_card__container'>{Projects}</div>
+    </section>
+  );
+};
+
+export default AllGames;
